587d6c47a69fd92f7ebbb5dcf677e2a96a6b9dd4
mark <code-git@marw.ca>
2025-03-02 19:35:36 -0500

    Initial commit

---
+++ src/main/java/edu/exampleuni/ums/NavController.java
@@ -0,0 +1,12 @@
+package edu.exampleuni.ums;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class NavController {
+    // @ FXML
+    // private Label welcomeText;
+
+    // @ FXML
+    // protected void onHelloButtonClick() { welcomeText.setText("Welcome to JavaFX Application!"); }
+}
---
+++ src/main/java/edu/exampleuni/ums/Window.java
@@ -0,0 +1,23 @@
+package edu.exampleuni.ums;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Window extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Window.class.getResource("Window.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
