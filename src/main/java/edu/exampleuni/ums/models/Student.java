697b13a639b0ec3dfcc2092ba8d6b3616b4cc141
Uranocene238 <nealkudver@gmail.com>
2025-03-31 22:45:46 +0000

    Create Student.java
    Student should be an inherited class from user

---
+++ src/main/java/edu/exampleuni/ums/models/Student.java
@@ -0,0 +1,58 @@
+package edu.exampleuni.ums.models;
+
+import java.util.ArrayList;
+
+public class Student extends User {
+    private String Address;
+    private String TelephoneNumber;
+    private String AcademicLevel;
+    private String CurrentSemester;
+    private String photoLocation;
+    private ArrayList<String> Courses;
+    private String ThesisTitle;
+    private int progress;
+
+    public Student(String name, String email, byte[] password, String address, String TelephoneNumber, String AcademicLevel, String CurrentSemester, String photoLocation, ArrayList<String> Courses, String ThesisTitle, int progress, ArrayList<String> fullName) {
+        super(name, email, password, fullName);
+        this.Address = address;
+        this.TelephoneNumber = TelephoneNumber;
+        this.AcademicLevel = AcademicLevel;
+        this.CurrentSemester = CurrentSemester;
+        this.photoLocation = photoLocation;
+        this.Courses = Courses;
+        this.ThesisTitle = ThesisTitle;
+        this.progress = progress;
+        this.role = USER;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+    public String getTelephoneNumber() {
+        return TelephoneNumber;
+    }
+
+    public String getThesisTitle() {
+        return ThesisTitle;
+    }
+
+    public ArrayList<String> getCourses() {
+        return Courses;
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public String getCurrentSemester() {
+        return CurrentSemester;
+    }
+
+    public String getAcademicLevel() {
+        return AcademicLevel;
+    }
+
+    public String getPhotoLocation() {
+        return photoLocation;
+    }
+}
