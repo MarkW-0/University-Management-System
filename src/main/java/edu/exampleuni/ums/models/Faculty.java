0061cf2329a58e94204bb313ffab4e040e20d513
Uranocene238 <nealkudver@gmail.com>
2025-03-31 22:36:20 +0000

    Update Faculty.java

--- src/main/java/edu/exampleuni/ums/models/Faculty.java
+++ src/main/java/edu/exampleuni/ums/models/Faculty.java
@@ -1,17 +1,35 @@
 package edu.exampleuni.ums.models;
 
+import java.util.ArrayList;
 
 public class Faculty extends User {
-	/*	todo
-		Degree (e.g., Ph.D., Master's).
-		Research Interest
-		Office
-	*/
-	public Faculty(String id, String fullName, String email) {
-		super();
-		this.setRole(Role.FACULTY);
-		this.setID(id);
-		this.setFullName(fullName);
-		this.setEmail(email);
-	}
+    private String degree;
+    private String researchInterest;
+    private  String officeLocation;
+    // Don't know what's the max courses a faculty member can offer, probably will change later
+    private ArrayList<String> coursesOffered;
+
+    public ArrayList<String> getCoursesOffered() {
+        return coursesOffered;
+    }
+
+    public String getResearchInterest() {
+        return researchInterest;
+    }
+
+    public String getOfficeLocation() {
+        return officeLocation;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+    //
+    public Faculty(String name, String email, byte[] password, String degree, String researchInterest, String officeLocation, ArrayList<String> fullName){
+        super(name, email, password, fullName);
+        this.degree = degree;
+        this.researchInterest = researchInterest;
+        this.officeLocation = officeLocation;
+        this.userRole = "ADMIN";
+    }
 }
