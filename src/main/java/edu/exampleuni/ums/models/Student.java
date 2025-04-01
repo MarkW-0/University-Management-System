e4245b2a705efd67c2576d2972c1cee956b0fd92
Uranocene238 <nealkudver@gmail.com>
2025-04-01 04:44:31 +0000

    Update Student.java

--- src/main/java/edu/exampleuni/ums/models/Student.java
+++ src/main/java/edu/exampleuni/ums/models/Student.java
@@ -12,8 +12,8 @@ public class Student extends User {
     private String ThesisTitle;
     private int progress;
 
-    public Student(String name, String email, byte[] password, String address, String TelephoneNumber, String AcademicLevel, String CurrentSemester, String photoLocation, ArrayList<String> Courses, String ThesisTitle, int progress, ArrayList<String> fullName) {
-        super(name, email, password, fullName);
+    public Student(String name, String email, byte[] password, String address, String TelephoneNumber, String AcademicLevel, String CurrentSemester, String photoLocation, ArrayList<String> Courses, String ThesisTitle, int progress, ArrayList<String> fullName, String id) {
+        super(name, email, password, fullName, id);
         this.Address = address;
         this.TelephoneNumber = TelephoneNumber;
         this.AcademicLevel = AcademicLevel;
