09076751e93ee5aa02cef347f2bcd380ec54ac8b
numnu <numnuggettwin@gmail.com>
2025-04-01 01:11:21 -0400

    change user service that defines deafult users to match with new user constructor

--- src/main/java/edu/exampleuni/ums/services/UserService.java
+++ src/main/java/edu/exampleuni/ums/services/UserService.java
@@ -2,16 +2,17 @@ package edu.exampleuni.ums.services;
 
 import edu.exampleuni.ums.models.Role;
 import edu.exampleuni.ums.models.User;
-import java.nio.charset.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class UserService extends Service<User> {
 	public UserService() {
 		super(new ArrayList<>(Arrays.asList(
-				new User(Role.ADMIN, "admin", "admin".getBytes(StandardCharsets.UTF_8), "ADMIN", "admin@exampleuni.edu"),
-				new User(Role.FACULTY, "faculty", "faculty".getBytes(StandardCharsets.UTF_8), "FACULTY", "faculty@exampleuni.edu"),
-				new User(Role.STUDENT, "student", "student".getBytes(StandardCharsets.UTF_8), "STUDENT", "student@exampleuni.edu"),
-				new User(Role.USER, "user", "user".getBytes(StandardCharsets.UTF_8), "USER", "user@exampleuni.edu")
+				// Updated User instances using the new constructor
+				new User("admin", "admin@exampleuni.edu", "admin".getBytes(StandardCharsets.UTF_8), new ArrayList<>(Arrays.asList("Admin")), "ADMIN"),
+				new User("faculty", "faculty@exampleuni.edu", "faculty".getBytes(StandardCharsets.UTF_8), new ArrayList<>(Arrays.asList("Faculty")), "FACULTY"),
+				new User("student", "student@exampleuni.edu", "student".getBytes(StandardCharsets.UTF_8), new ArrayList<>(Arrays.asList("Student")), "STUDENT"),
+				new User("user", "user@exampleuni.edu", "user".getBytes(StandardCharsets.UTF_8), new ArrayList<>(Arrays.asList("User")), "USER")
 		)));
 	}
 }
