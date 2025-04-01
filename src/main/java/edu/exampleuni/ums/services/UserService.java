7205c7e11b3f0148e187d196efdcf71eecc84ace
Shane <nealkudver@gmail.com>
2025-04-01 01:28:07 -0400

    Fix UserService constructor

--- src/main/java/edu/exampleuni/ums/services/UserService.java
+++ src/main/java/edu/exampleuni/ums/services/UserService.java
@@ -9,10 +9,10 @@ public class UserService extends Service<User> {
 	public UserService() {
 		super(new ArrayList<>(Arrays.asList(
 				// Updated User instances using the new constructor
-				new User("admin", "admin@exampleuni.edu", "admin".getBytes(StandardCharsets.UTF_8), new ArrayList<>(Arrays.asList("Admin")), "ADMIN"),
-				new User("faculty", "faculty@exampleuni.edu", "faculty".getBytes(StandardCharsets.UTF_8), new ArrayList<>(Arrays.asList("Faculty")), "FACULTY"),
-				new User("student", "student@exampleuni.edu", "student".getBytes(StandardCharsets.UTF_8), new ArrayList<>(Arrays.asList("Student")), "STUDENT"),
-				new User("user", "user@exampleuni.edu", "user".getBytes(StandardCharsets.UTF_8), new ArrayList<>(Arrays.asList("User")), "USER")
+				new User("admin", "admin@exampleuni.edu", "admin".getBytes(StandardCharsets.UTF_8), "Admin" , "ADMIN"),
+				new User("faculty", "faculty@exampleuni.edu", "faculty".getBytes(StandardCharsets.UTF_8), "Faculty", "FACULTY"),
+				new User("student", "student@exampleuni.edu", "student".getBytes(StandardCharsets.UTF_8), "Student", "STUDENT"),
+				new User("user", "user@exampleuni.edu", "user".getBytes(StandardCharsets.UTF_8), "User", "USER")
 		)));
 	}
 }
